a783ee83a860d3c47ffa30b0707929eb5e382203
shohan-pherones <shohan.sub.56@gmail.com>
2022-11-14 21:34:54 +0600

    lenis strt added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
+import Lenis from "@studio-freight/lenis";
 import Navbar from "./components/Navbar";
 import Socials from "./components/Socials";
 import Home from "./components/Home";
@@ -7,6 +9,22 @@ import Footer from "./components/Footer";
 import Blogs from "./components/Blogs";
 
 const App = () => {
+  const lenis = new Lenis({
+    duration: 1.5,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    direction: "vertical",
+    smooth: true,
+  });
+
+  useEffect(() => {
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
+
   return (
     <div className="app">
       <div className="noise"></div>
