9d79417b374498d21becb46850f7540935994dec
shohan-pherones <shohan.sub.56@gmail.com>
2022-12-05 19:28:00 +0600

    some bug fixed and new build added

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
+  <BrowserRouter>
+    <React.StrictMode>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    </React.StrictMode>
+  </BrowserRouter>
 );
