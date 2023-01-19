e28b8613798ecc30123de2a98e4c6b34afb3612b
shohan-pherones <shohan.sub.56@gmail.com>
2023-01-19 13:06:29 +0600

    skills tree updated

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,75 +1,104 @@
+const data = [
+  {
+    id: 1,
+    skill: "HTML",
+  },
+  {
+    id: 2,
+    skill: "CSS",
+  },
+  {
+    id: 3,
+    skill: "SASS",
+  },
+  {
+    id: 4,
+    skill: "Bootstrap",
+  },
+  {
+    id: 5,
+    skill: "Tailwind CSS",
+  },
+  {
+    id: 6,
+    skill: "JavaScript",
+  },
+  {
+    id: 7,
+    skill: "GSAP",
+  },
+  {
+    id: 8,
+    skill: "React.js",
+  },
+  {
+    id: 9,
+    skill: "React Router",
+  },
+  {
+    id: 10,
+    skill: "Axios",
+  },
+  {
+    id: 11,
+    skill: "Redux.js",
+  },
+  {
+    id: 12,
+    skill: "Redux Toolkit",
+  },
+  {
+    id: 13,
+    skill: "Firebase",
+  },
+  {
+    id: 14,
+    skill: "Node.js",
+  },
+  {
+    id: 15,
+    skill: "Express.js",
+  },
+  {
+    id: 16,
+    skill: "MongoDB",
+  },
+];
+
 const Skills = () => {
   return (
     <div className="skills container mx-auto mt-40" id="skills">
       <h2 className="section-title">My Skills</h2>
       <div className="skills-wrapper grid grid-cols-1 sm:grid-cols-2 gap-10 sm:gap-20 mt-40">
         <ul className="skills-wrapper-1 flex flex-col gap-10">
-          <li className="skill-item flex gap-10 items-baseline">
-            <span className="skill-number text-white/50">01.</span>
-            <span className="skill-name">HTML</span>
-          </li>
-          <li className="skill-item flex gap-10 items-baseline">
-            <span className="skill-number text-white/50">02.</span>
-            <span className="skill-name">CSS</span>
-          </li>
-          <li className="skill-item flex gap-10 items-baseline">
-            <span className="skill-number text-white/50">03.</span>
-            <span className="skill-name">SASS</span>
-          </li>
-          <li className="skill-item flex gap-10 items-baseline">
-            <span className="skill-number text-white/50">04.</span>
-            <span className="skill-name">Bootstrap</span>
-          </li>
-          <li className="skill-item flex gap-10 items-baseline">
-            <span className="skill-number text-white/50">05.</span>
-            <span className="skill-name">Tailwind CSS</span>
-          </li>
-          <li className="skill-item flex gap-10 items-baseline">
-            <span className="skill-number text-white/50">06.</span>
-            <span className="skill-name">JavaScript</span>
-          </li>
-          <li className="skill-item flex gap-10 items-baseline">
-            <span className="skill-number text-white/50">07.</span>
-            <span className="skill-name">GSAP</span>
-          </li>
-          <li className="skill-item flex gap-10 items-baseline">
-            <span className="skill-number text-white/50">08.</span>
-            <span className="skill-name">React.js</span>
-          </li>
+          {data
+            .filter((_, i) => i < Math.floor(data.length / 2))
+            .map((skill) => (
+              <li
+                key={skill.id}
+                className="skill-item flex gap-10 items-baseline"
+              >
+                <span className="skill-number text-white/50">
+                  {String(skill.id).padStart(2, 0).padEnd(3, ".")}
+                </span>
+                <span className="skill-name">{skill.skill}</span>
+              </li>
+            ))}
         </ul>
         <ul className="skills-wrapper-2 flex flex-col gap-10">
-          <li className="skill-item flex gap-10 items-baseline">
-            <span className="skill-number text-white/50">09.</span>
-            <span className="skill-name">React Router</span>
-          </li>
-          <li className="skill-item flex gap-10 items-baseline">
-            <span className="skill-number text-white/50">10.</span>
-            <span className="skill-name">Axios</span>
-          </li>
-          <li className="skill-item flex gap-10 items-baseline">
-            <span className="skill-number text-white/50">11.</span>
-            <span className="skill-name">Redux.js</span>
-          </li>
-          <li className="skill-item flex gap-10 items-baseline">
-            <span className="skill-number text-white/50">12.</span>
-            <span className="skill-name">Redux Toolkit</span>
-          </li>
-          <li className="skill-item flex gap-10 items-baseline">
-            <span className="skill-number text-white/50">13.</span>
-            <span className="skill-name">Firebase</span>
-          </li>
-          <li className="skill-item flex gap-10 items-baseline">
-            <span className="skill-number text-white/50">14.</span>
-            <span className="skill-name">Node.js</span>
-          </li>
-          <li className="skill-item flex gap-10 items-baseline">
-            <span className="skill-number text-white/50">15.</span>
-            <span className="skill-name">Express.js</span>
-          </li>
-          <li className="skill-item flex gap-10 items-baseline">
-            <span className="skill-number text-white/50">16.</span>
-            <span className="skill-name">MongoDB</span>
-          </li>
+          {data
+            .filter((_, i) => i >= Math.floor(data.length / 2))
+            .map((skill) => (
+              <li
+                key={skill.id}
+                className="skill-item flex gap-10 items-baseline"
+              >
+                <span className="skill-number text-white/50">
+                  {String(skill.id).padStart(2, 0).padEnd(3, ".")}
+                </span>
+                <span className="skill-name">{skill.skill}</span>
+              </li>
+            ))}
         </ul>
       </div>
     </div>
