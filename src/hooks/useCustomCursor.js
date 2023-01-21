b24832c3bbb9069342d28ca4f104d0b618db10aa
shohan-pherones <shohan.sub.56@gmail.com>
2023-01-21 15:58:04 +0600

    custom curosor grows on links

--- src/hooks/useCustomCursor.js
+++ src/hooks/useCustomCursor.js
@@ -2,6 +2,8 @@ import { useEffect } from "react";
 
 export const useCustomCursor = (innerCursor, outerCursor) => {
   useEffect(() => {
+    document.addEventListener("mousemove", moveCursor);
+
     function moveCursor(e) {
       let x = e.clientX;
       let y = e.clientY;
@@ -12,6 +14,16 @@ export const useCustomCursor = (innerCursor, outerCursor) => {
       outerCursor.current.style.top = `${y}px`;
     }
 
-    document.addEventListener("mousemove", moveCursor);
+    let links = Array.from(document.querySelectorAll("a"));
+
+    links.forEach((link) => {
+      link.addEventListener("mouseover", () => {
+        innerCursor.current.classList.add("grow");
+      });
+
+      link.addEventListener("mouseleave", () => {
+        innerCursor.current.classList.remove("grow");
+      });
+    });
   }, [innerCursor, outerCursor]);
 };
